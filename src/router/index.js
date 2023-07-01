51a48dd8380b49eadab98dbd1ed311f1cf85b662
Chen <463363269@qq.com>
2023-07-01 14:03:03 +0800

    4.1

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,48 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+
+/**
+ * 几点说明：
+ * 1. required
+ *    path: 路由路径
+ *    component: 路由路径对应的路由组件
+ * 2. name: 路由名称
+*/
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+
+    // 按需加载，节约性能。
+    // 若没有访问 /about，则不会加载 AboutView 这个组件
+    component: () => import('../views/AboutView.vue'),
+  },
+  {
+    path: '/demo',
+    name: 'demo',
+    component: () => import('../views/DemoView.vue'),
+  },
 ]
 
+// 创建路由对象
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 })
+
+/**
+ * createWebHashHistory：url 上多了个 #
+ * createWebHisttory：url 上没有 #
+ * 
+ * 比如本地访问 /
+ * 前者表现为：http://localhost:8080/#/
+ * 后者表现为：http://localhost:8080
+*/
 
 export default router
